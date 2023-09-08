5f932b6d2f1e40cda014612659b3f9bdac16c6c3
rozsnono <rozsnono@gmail.com>
2023-09-08 12:18:12 +0200

    pmszt

--- src/app/pmszt/page.tsx
+++ src/app/pmszt/page.tsx
@@ -1,5 +1,75 @@
+"use client";
+import QuestionTab from "@/components/questionTab";
+import PMSZT from "../../txts/pmszt.json";
+import { useQuery } from "react-query";
+import { useState } from "react";
+
 export default function Home() {
-    return(
-        <main></main>
+
+    function getData() {
+        try {
+            const data = PMSZT.data;
+            if (sessionStorage.getItem("queue") != "undefined" && sessionStorage.getItem("queue") != null) {
+                const qu: string | any = sessionStorage.getItem("queue");
+                setQueue(JSON.parse(qu) as any)
+            } else {
+                setQueue(queuing(data));
+            }
+            if (!Number.isNaN(parseFloat(sessionStorage.getItem("number") as string))) {
+                const n: number | any = sessionStorage.getItem("number");
+                setNumber(parseFloat(n))
+            }
+            return data;
+        } catch (error: Error | any) {      //muszáj így megadni, mert különben hibát dob
+            console.error(error);
+            return error;
+        }
+
+    }
+
+    function queuing(data: any) {
+        let array: any = [];
+        for (let index = 0; index < 24; index++) {
+            let random = getRandomInt(data.length)
+            if (!array.includes(random)) {
+                array.push(random);
+            } else {
+                index--;
+            }
+        }
+        sessionStorage.setItem("queue", JSON.stringify(array))
+        return array;
+    }
+
+    function getRandomInt(max: any) {
+        return Math.floor(Math.random() * max);
+    }
+
+    const [queue, setQueue] = useState([]);
+
+    const pmszt: any = useQuery('subjects', getData);
+
+    const [number, setNumber] = useState(0);
+
+    function Next(item: any) {
+        setNumber(number + 1);
+        sessionStorage.setItem("number", (number + 1).toString());
+        if(number + 1 > 23){
+            sessionStorage.removeItem("number");
+            sessionStorage.removeItem("queue");
+            // window.location.href = "/pmszt/result";
+            setNumber(0);setQueue(queuing(pmszt.data));
+        }
+    }
+
+
+    return (
+        <main>
+            {
+                !pmszt.isLoading ?
+                    <QuestionTab img={pmszt.data[queue[number]].pic} question={pmszt.data[queue[number]].question} number={number + 1} answers={pmszt.data[queue[number]].options} next={(e) => { Next(e); }} type={pmszt.data[queue[number]].answers.length > 1 ? "checkbox" : "radio"}></QuestionTab>
+                    : <></>
+            }
+        </main>
     );
 }
--- src/components/questionTab.tsx
+++ src/components/questionTab.tsx
@@ -1,12 +1,17 @@
 import { Button } from "primereact/button";
-import { Slider } from "primereact/slider";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import { RadioButton } from "primereact/radiobutton";
+import { Checkbox } from "primereact/checkbox";
 
-export default function Tab({ question, answers, number, type, next }: { question: string, answers: any, number: number, type: string, next: (e: any) => void }) {
+export default function Tab({ question, answers, number, type, img, next }: { question: string, answers: any, number: number, img?: string | null, type: string, next: (e: any) => void }) {
     const colors: Array<any> = ['success', 'info', 'warning', 'danger'];
 
     const [choosed, setChoosed] = useState<any>([]);
+
+    function toBase64(arr: any) {
+        return arr.replace("PIC:", "data:image/png;base64,");
+    }
 
     return (
         <main>
@@ -27,19 +32,34 @@ export default function Tab({ question, answers, number, type, next }: { questio
                             <div className="flex flex-col space-y-1 text-center relative">
                                 <div className="text-lg text-end "> <span className="text-gray-800 font-bold">{number}</span> <span className="text-gray-400">of</span> <span className="text-gray-600">24</span> </div>
                             </div>
+                            {
+                                img === null || img === "" ?
+                                    <></> :
+                                    <div className="flex flex-col space-y-1 text-center relative items-center jusitfy-center">
+                                        <Image src={img?toBase64(img):""} width={300} height={100} alt=""></Image>
+                                    </div>
+                            }
                             <div className='grid gap-5 px-6 pb-6'>
                                 {
-                                    answers.map((a: any, i: number) => (
-                                        <div key={i} className="flex align-items-center">
-                                            <RadioButton inputId={a} name="category" value={a} onChange={(e) => setChoosed(e.value)} checked={choosed === a} />
-                                            <label htmlFor={a} className="ml-2">{a}</label>
-                                        </div>
-                                    ))
+                                    type === "checkbox" ?
+                                        answers.map((a: any, i: number) => (
+                                            <div key={i} className="flex align-items-center">
+                                                <Checkbox inputId={a} value={a} onChange={(e) => setChoosed([...choosed, e.value])} checked={choosed.includes(a)} />
+                                                <label htmlFor={a} className="ml-2">{a}</label>
+                                            </div>
+                                        ))
+                                        :
+                                        answers.map((a: any, i: number) => (
+                                            <div key={i} className="flex align-items-center">
+                                                <RadioButton inputId={a} name="category" value={a} onChange={(e) => setChoosed(e.value)} checked={choosed === a} />
+                                                <label htmlFor={a} className="ml-2">{a}</label>
+                                            </div>
+                                        ))
                                 }
                             </div>
                             <div className="flex justify-between">
 
-                                <Button onClick={() => {next(choosed); setChoosed(null)}} rounded label="Check" icon="pi pi-send" severity='help' />
+                                <Button onClick={() => { next(choosed); setChoosed([]) }} rounded label="Check" icon="pi pi-send" severity='help' />
                             </div>
 
 
