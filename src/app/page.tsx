891717ac157ec93f8bbc75f9323f7bd66f3cc954
rozsnono <rozsnono@gmail.com>
2023-09-09 20:09:06 +0200

    mobile fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -71,7 +71,7 @@ export default function Home() {
   ];
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center lg:p-24 p-16">
       <Menus menuLeft={menuLeft} />
 
       <div className="flex flex-cols-2">
