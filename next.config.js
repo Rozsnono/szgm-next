40de316720bd24126ddc4091f2cbe549f3e89c9b
rozsnono <rozsnono@gmail.com>
2023-10-24 12:28:25 +0200

    server

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
         return [
             {
                 source: '/api/:path*',
-                destination: 'http://localhost:8000/api/:path*',
+                destination: 'https://teal-frail-ostrich.cyclic.app/api/:path*',
             },
         ];
     },
