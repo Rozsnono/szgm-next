53f4180a1290a75ace31e047725fb352a6d2947b
rozsnono <rozsnono@gmail.com>
2023-10-26 22:52:09 +0200

    asd

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -22,11 +22,30 @@ export default function Home() {
         return data;
     }
 
+    async function getUsers() {
+        const res = await fetch("https://teal-frail-ostrich.cyclic.app/api/users");
+        const data = await res.json();
+        return data;
+    }
+
+    function banUser() {
+        fetch("https://teal-frail-ostrich.cyclic.app/api/user/"+choosen._id+"/"+!choosen.isDeleted ,{ method: "DELETE" })
+    }
+
     const data = useQuery<any[]>('database', getData);
+    const users = useQuery<any[]>('users', getUsers);
+    const [choosen, setChoosen] = useState<any>(null);
 
     return (
         <main className="flex flex-col min-h-screen gap-2 lg:p-12 lg:pt-24 p-6 pt-32 text-sm">
-
+            {
+                !users.isLoading && users.data ?
+                    <div className="flex gap-2">
+                        <Dropdown value={choosen} onChange={(e) => setChoosen(e.value)} options={users.data} optionLabel="user"
+                            placeholder="Users" className="w-fit" />
+                        <div onClick={banUser} className="border border-blue-800 bg-blue-800 text-white flex items-center justify-center p-2 rounded-md cursor-pointer hover:bg-white hover:text-blue-800 duration-100">{choosen && choosen.isDeleted ? "Unban" : "Ban"}</div>
+                    </div> : <></>
+            }
             <div className="border border-black rounded-lg w-full h-full flex flex-col gap-1 bg-gray-200">
                 {!data.isLoading && data.data
                     ? data.data.map((item: any, index: number) => {
@@ -49,7 +68,7 @@ export default function Home() {
                                 <div>
                                     <p className="text-gray-500">{item.ip}</p>
                                 </div>
-                                
+
                             </div>
                         );
                     })
