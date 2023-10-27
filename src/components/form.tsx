8c94c0549afe474c39c0b87709463a677f70cfc7
rozsnono <rozsnono@gmail.com>
2023-10-27 17:00:11 +0200

    ipload

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -14,7 +14,7 @@ export default function Form({
     const [pass, setPass] = useState('');
 
     const { user, setUser } = useContext<any>(UserContext);
-    const [checkUser, setCheckUser] = useState<any | null>();
+    const [checkUser, setCheckUser] = useState<any | null>(undefined);
 
     const [message, setMessage] = useState<any>();
 
@@ -30,7 +30,7 @@ export default function Form({
     }
 
     async function login(user: string, password: string) {
-        let data = { ip: "" };
+        let data = { ip: "1.1.1.1" };
         try {
             const res = await fetch("https://api.ipify.org/?format=json");
             data = await res.json();
@@ -54,21 +54,26 @@ export default function Form({
         <>
             {
                 checkUser == null ?
-                    <main className="fixed top-0 p-2 flex justify-center w-screen h-screen bg-[#00000040]" style={{ zIndex: 1001 }}>
-                        <div className="mx-auto flex flex-col bg-white border-2 border-blue-800 gap-6 p-6 rounded-lg h-fit" >
-                            <span className="p-float-label">
-                                <InputText id="username" value={TMPuser} onChange={(e) => setTMPUser(e.target.value)} />
-                                <label htmlFor="username">Username</label>
-                            </span>
-                            <span className="p-float-label">
-                                <Password id="password" value={pass} onChange={(e) => setPass(e.target.value)} feedback={false} tabIndex={1} />
-                                <label htmlFor="password">Password</label>
-                            </span>
-                            <div className="text-red-500">{message}</div>
+                    <>
+                        {
+                            checkUser === null ? <></> :
+                                <main className="fixed top-0 p-2 flex justify-center w-screen h-screen bg-[#00000040]" style={{ zIndex: 1001 }}>
+                                    <div className="mx-auto flex flex-col bg-white border-2 border-blue-800 gap-6 p-6 rounded-lg h-fit" >
+                                        <span className="p-float-label">
+                                            <InputText id="username" value={TMPuser} onChange={(e) => setTMPUser(e.target.value)} />
+                                            <label htmlFor="username">Username</label>
+                                        </span>
+                                        <span className="p-float-label">
+                                            <Password id="password" value={pass} onChange={(e) => setPass(e.target.value)} feedback={false} tabIndex={1} />
+                                            <label htmlFor="password">Password</label>
+                                        </span>
+                                        <div className="text-red-500">{message}</div>
 
-                            <div className="w-full rounded-full border-blue-800 bg-white border-2 text-blue-800 hover:bg-blue-800 hover:text-white duration-100 text-center cursor-pointer" onClick={logining}>Login</div>
-                        </div>
-                    </main> :
+                                        <div className="w-full rounded-full border-blue-800 bg-white border-2 text-blue-800 hover:bg-blue-800 hover:text-white duration-100 text-center cursor-pointer" onClick={logining}>Login</div>
+                                    </div>
+                                </main>
+                        }
+                    </> :
                     children
             }
         </>
