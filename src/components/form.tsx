111d30d9962775880db2d636bc5f7b4d894a81bf
rozsnono <rozsnono@gmail.com>
2023-10-27 18:09:27 +0200

    loading

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -19,16 +19,14 @@ export default function Form({
     const [message, setMessage] = useState<any>();
 
     useEffect(() => {
-        if(user !== undefined){
+        if (user !== undefined) {
             setCheckUser(user);
         }
-    },[user])
+    }, [user])
 
 
     async function logining() {
         login(TMPuser, pass);
-        // console.log(await login());
-
     }
 
     async function login(user: string, password: string) {
@@ -58,7 +56,12 @@ export default function Form({
                 checkUser == null ?
                     <>
                         {
-                            checkUser === undefined ? <></> :
+                            checkUser === undefined ?
+                                <main className="fixed top-0 p-2 flex justify-center w-screen h-screen bg-[#00000040]" style={{ zIndex: 1001 }}>
+                                    <div className="mx-auto flex flex-col bg-white border-2 border-blue-800 gap-6 p-6 rounded-lg h-fit" >
+                                        <i className="pi pi-spin pi-spinner" style={{ fontSize: "3rem" }}></i>
+                                    </div>
+                                </main> :
                                 <main className="fixed top-0 p-2 flex justify-center w-screen h-screen bg-[#00000040]" style={{ zIndex: 1001 }}>
                                     <div className="mx-auto flex flex-col bg-white border-2 border-blue-800 gap-6 p-6 rounded-lg h-fit" >
                                         <span className="p-float-label">
