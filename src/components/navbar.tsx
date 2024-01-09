e62ea66ae23a5f508ca3f0070f903632765b58d9
rozsnono <rozsnono@gmail.com>
2024-01-09 11:05:34 +0100

    asd

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -90,7 +90,7 @@ export default function Navbar({
                         <button onClick={() => { setShow(true) }} className='px-4 py-2 border-2 rounded-xl border-blue-800 hover:border-blue-900 hover:bg-blue-900 text-white'><i className='pi pi-user'></i></button> : <></>
                 }
                 {
-                    checkUser && checkUser.role > 3 ?
+                    checkUser && checkUser.role < 3 ?
                         <button onClick={() => { router.replace("/admin"); }} className='px-4 py-2 border-2 rounded-xl border-blue-800 hover:border-blue-900 hover:bg-blue-900 text-white'><i className='pi pi-users'></i></button> : <></>
                 }
                 {
