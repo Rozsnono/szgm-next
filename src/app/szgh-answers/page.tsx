349b70089cb669c13145cedfbe8f77b55d04f5dd
rozsnono <rozsnono@gmail.com>
2024-05-14 20:12:10 +0200

    szgh

--- src/app/szgh-answers/page.tsx
+++ src/app/szgh-answers/page.tsx
@@ -10,9 +10,6 @@ export default function Home() {
     function getData() {
         try {
             let data = SZGH.data;
-            data = data.sort(function (a, b) {
-                return 0.5 - Math.random();
-            });
             return data;
         } catch (error: Error | any) {      //muszáj így megadni, mert különben hibát dob
             console.error(error);
@@ -30,14 +27,29 @@ export default function Home() {
 
     function Next(item: any) {
 
-        setQueue(queue=> queue + 1);
+        setQueue(queue => queue + 1);
     }
+
+    function setShow(){
+        setShowAnswer(!showAnswer);
+        setQueue(queue => queue);
+    }
+
+    const [showAnswer, setShowAnswer] = useState<boolean>(true);
 
     return (
         <main className="w-screen h-screen flex items-center justify-center">
+
+            <button type="button" onClick={setShow} className={`flex fixed top-20 left-5 items-center gap-2 text-white focus:outline-none focus:ring-4 font-medium rounded-full text-md px-5 py-2.5 text-center me-2 mb-2 duration-200 ${!showAnswer&&"bg-red-700 hover:bg-red-800 focus:ring-red-300"} ${showAnswer&&"bg-blue-700 hover:bg-blue-800 focus:ring-blue-300"}`}>{!showAnswer?"Mutasd":"Rejtsd el"} a választ! <i className="pi pi-search"></i> </button>
+
             {
                 !szgh.isLoading && szgh.data.length > queue ?
-                    <QuestionTab key={queue} finished={(e) => { }} max={szgh.data.length} icon="sitemap" type="radio" question={szgh.data[queue].question} number={queue + 1} answers={["Igaz", "Hamis"]} result={szgh.data[queue].answer===true?["Igaz"]:["Hamis"]} correct={szgh.data[queue].answer===true?["Igaz"]:["Hamis"]} next={Next} checking={true} ></QuestionTab>
+                    <QuestionTab key={queue} finished={(e) => { }}
+                        max={szgh.data.length} icon="sitemap" type="radio"
+                        question={szgh.data[queue].question} number={queue + 1} answers={["Igaz", "Hamis"]}
+                        result={showAnswer ? (szgh.data[queue].answer === true ? ["Igaz"] : ["Hamis"]) : []}
+                        correct={showAnswer ? (szgh.data[queue].answer === true ? ["Igaz"] : ["Hamis"]) : []}
+                        next={Next} checking={true} ></QuestionTab>
                     : <></>
             }
         </main>
