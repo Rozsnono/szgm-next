9153a3ce93b9518c559c597df0c315b4599b4501
rozsnorbert <norbert.rozs@coincash.eu>
2025-01-28 14:43:55 +0100

    no timer

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,22 +3,11 @@ import { useEffect, useState } from "react";
 
 
 export default function Loading() {
-
-
-    const [time, setTime] = useState(1);
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setTime((prev) => prev + 1);
-        }, 1000);
-    }, []);
-
     return (
         <main className="min-h-screen w-screen  bg-white absolute top-0 left-0">
             <div className="flex items-center min-h-screen p-4  bg-white justify-center">
                 <i className="pi pi-spinner pi-spin text-gray-500 absolute " style={{ fontSize: "10rem" }}></i>
                 <div className="text-gray-500 text-3xl">
-                    {time}s
                 </div>
             </div>
         </main>
