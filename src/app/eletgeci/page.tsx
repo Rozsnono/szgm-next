3c9cee057dad11be17e4853772316eed6b1287b1
rozsnorbert <norbert.rozs@coincash.eu>
2025-05-15 00:16:51 +0200

    eletgeci 2

--- src/app/eletgeci/page.tsx
+++ src/app/eletgeci/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import QuestionTab from "@/components/questionTab";
-import PSZICHO from "../../txts/pszicho.json";
+import eletgeci from "../../txts/eletgeci.json";
 import { useQuery } from "react-query";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
@@ -9,7 +9,7 @@ export default function Home() {
 
     function getData() {
         try {
-            const data = PSZICHO;
+            const data = eletgeci;
             if (sessionStorage.getItem("queue") != "undefined" && sessionStorage.getItem("queue") != null) {
                 const qu: string | any = sessionStorage.getItem("queue");
                 setQueue(JSON.parse(qu) as any)
@@ -48,7 +48,7 @@ export default function Home() {
 
     const [queue, setQueue] = useState([]);
 
-    const pszicho: any = useQuery('pszicho', getData);
+    const eletgeci: any = useQuery('eletgeci', getData);
 
     const [number, setNumber] = useState(0);
 
@@ -56,35 +56,35 @@ export default function Home() {
     function Next(item: any) {
         setNumber(number + 1);
         sessionStorage.setItem("number", (number + 1).toString());
-        let result = sessionStorage.getItem("pszicho-result") ? JSON.parse(sessionStorage.getItem("pszicho-result") || "") : [];
-        result.push({ question: pszicho.data[queue[number]].question, answer: item, options: pszicho.data[queue[number]].options, correct: pszicho.data[queue[number]].answers, type: pszicho.data[queue[number]].answers.length > 1 ? "checkbox" : "radio" });
-        sessionStorage.setItem("pszicho-result", JSON.stringify(result));
+        let result = sessionStorage.getItem("eletgeci-result") ? JSON.parse(sessionStorage.getItem("eletgeci-result") || "") : [];
+        result.push({ question: eletgeci.data[queue[number]].question, answer: item, options: eletgeci.data[queue[number]].options, correct: eletgeci.data[queue[number]].answers, type: eletgeci.data[queue[number]].answers.length > 1 ? "checkbox" : "radio" });
+        sessionStorage.setItem("eletgeci-result", JSON.stringify(result));
         if (number + 1 > 29) {
             sessionStorage.removeItem("number");
             sessionStorage.removeItem("queue");
-            router.push("/results?exam=pszicho");
+            router.push("/results?exam=eletgeci");
             setNumber(0);
         }
     }
 
     function finished(item: any) {
 
-        let result = sessionStorage.getItem("pszicho-result") ? JSON.parse(sessionStorage.getItem("pszicho-result") || "") : [];
-        result.push({ question: pszicho.data[queue[number]].question, answer: item, options: pszicho.data[queue[number]].options, correct: pszicho.data[queue[number]].answers, type: pszicho.data[queue[number]].answers.length > 1 ? "checkbox" : "radio" });
-        sessionStorage.setItem("pszicho-result", JSON.stringify(result));
+        let result = sessionStorage.getItem("eletgeci-result") ? JSON.parse(sessionStorage.getItem("eletgeci-result") || "") : [];
+        result.push({ question: eletgeci.data[queue[number]].question, answer: item, options: eletgeci.data[queue[number]].options, correct: eletgeci.data[queue[number]].answers, type: eletgeci.data[queue[number]].answers.length > 1 ? "checkbox" : "radio" });
+        sessionStorage.setItem("eletgeci-result", JSON.stringify(result));
         sessionStorage.removeItem("number");
         sessionStorage.removeItem("queue");
-        router.push("/results?exam=pszicho");
+        router.push("/results?exam=eletgeci");
         setNumber(0);
     }
 
 
     return (
         <main className="lg:pt-0 pt-12">
-            {!pszicho.isLoading && <>{console.log(pszicho.data, queue)}</>}
+            {!eletgeci.isLoading && <>{console.log(eletgeci.data, queue)}</>}
             {
-                !pszicho.isLoading && queue.length > 0 ?
-                    <QuestionTab max={30} icon="table" question={pszicho.data[queue[number]].question} number={number + 1} answers={pszicho.data[queue[number]].options} next={(e) => { Next(e); }} finished={(e) => { finished(e) }} type={pszicho.data[queue[number]].answers.length > 1 ? "checkbox" : "radio"}></QuestionTab>
+                !eletgeci.isLoading && queue.length > 0 ?
+                    <QuestionTab max={30} icon="table" question={eletgeci.data[queue[number]].question} number={number + 1} answers={eletgeci.data[queue[number]].options} next={(e) => { Next(e); }} finished={(e) => { finished(e) }} type={eletgeci.data[queue[number]].answers.length > 1 ? "checkbox" : "radio"}></QuestionTab>
                     : <></>
             }
         </main>
