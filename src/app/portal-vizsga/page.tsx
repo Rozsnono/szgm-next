2ab06e251eed50a379479ffd623aa22fbd4454cd
rozsnorbert <norbert.rozs@coincash.eu>
2025-05-29 20:22:18 +0200

    portal

---
+++ src/app/portal-vizsga/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+import QuestionTab from "@/components/questionTab";
+import PMSZT from "../../txts/portal.json";
+import { useQuery } from "react-query";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+
+    function getData() {
+        try {
+            const data = PMSZT;
+
+            return data;
+        } catch (error: Error | any) {      //muszáj így megadni, mert különben hibát dob
+            console.error(error);
+            return error;
+        }
+
+    }
+
+    const pmszt: any = useQuery('portal-vizsga', getData);
+
+
+    return (
+        <main className="lg:pt-12 pt-12">
+
+            {
+                !pmszt.isLoading &&
+                pmszt.data.map((item: any, index: number) => {
+                    return (
+                        // <QuestionTab finished={(e) => { }} key={index} icon={selectedExam.icon.replace("pi pi-", "")} type={item.type} question={item.question} number={index + 1} answers={item.options} result={item.answer} correct={item.correct} next={(e) => { }} ></QuestionTab>
+                        <div key={index}>
+                            {index % 30 === 0 && <hr/>}
+                            <QuestionTab key={index} max={30} icon="database" img={item.pic} result={item.answers} correct={item.answers} question={item.question} number={index + 1} answers={item.options} next={(e) => { }} finished={(e) => { }} type={item.answers.length > 1 ? "checkbox" : "radio"}></QuestionTab>
+                        </div>
+                    )
+                })
+
+            }
+        </main>
+    );
+}
